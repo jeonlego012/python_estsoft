2e10745371512b9119fc4c88a99aac13305261a3
jeonlego012 <jyo9873@gmail.com>
2024-07-11 22:44:27 +0900

    add 240711 quiz

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['main'],
+        'DIRS': ['main','login'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
