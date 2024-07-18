f2516d2221810e27fad6dc4758989ac056feaca0
jeonlego012 <jyo9873@gmail.com>
2024-07-18 23:49:27 +0900

    add 240718 lecture(cookie, session)

--- django/authprj/authapp/views.py
+++ django/authprj/authapp/views.py
@@ -4,11 +4,29 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    res = render(request, 'authapp/index.html')
+    
+    # cookie
+    # print(request.COOKIES)
+    # cookie_name = 'django_cookie_name'
+    # cookie_value = 'django_cookie_value 5678'
+    # res.set_cookie(cookie_name, cookie_value)
+    
+    # session
+    print(request.session)
+    
+    return res
 
 def signin(request):
     if request.method == "GET":
-        return render(request, 'signin.html')
+        if 'username' in request.COOKIES.keys():
+            context = {
+                'username': request.COOKIES['username'],
+            }
+            return render(request, 'signin.html', context)
+        else:
+            return render(request, 'signin.html')
+        
     elif request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -17,9 +35,18 @@ def signin(request):
         user = authenticate(username=username, password=password)
         
         if user is not None :
+            redi = redirect('authapp:index')
+            # cookie
+            redi.set_cookie('username', user.username, max_age=30)
+            
             # https://docs.djangoproject.com/en/5.0/topics/auth/default/#how-to-log-a-user-in
             login(request, user)
-            return redirect('authapp:index')
+            
+            # session
+            request.session['login_user'] = username
+            request.session.set_expiry(30)
+            return redi
+            
         else :
             # 유저 인증 실패
             context = {
@@ -35,10 +62,12 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
         
-        User.objects.create_user(username=username, password=password)
-
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        
         return redirect('authapp:index')
     
 def signout(request):
-    logout(request)
+    # logout(request)
+    request.session.flush()
     return redirect('authapp:index')
